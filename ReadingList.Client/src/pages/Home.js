4cc86e41f34259512880e674f47f2a3cf5ac010f
Szymon Głowacki <pgszymon@gmail.com>
2022-12-12 17:30:57 +0100

    Update Home page

--- ReadingList.Client/src/pages/Home.js
+++ ReadingList.Client/src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <h1>Home</h1>
+    <div className="text-center">
+      <h1 className="display-4 mb-4">Home</h1>
+      <p>
+        This project allows user to add <Link to="/books">books</Link> and <Link to="/authors">authors</Link> to database and create own <Link to="reading">reading list</Link>
+      </p>
+      <p>
+        You can find the description of this project on <a href="https://github.com/ZaleX0/ReadingList">github</a>
+      </p>
+    </div>
   )
 }
 
